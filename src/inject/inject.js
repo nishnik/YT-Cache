270da1a7922f02efeff143bfcfabe6cae4436348
Nishant Nikhil <nishantiam@gmail.com>
2017-08-13 20:03:05 +0530

    Temporarily working

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -10,16 +10,42 @@ function insert_main() {
         document.documentElement.appendChild(s)
     })(window.document);
 }
+function insert_video_js() {
+    (function(document) {
+        "use strict";
+        var s = document.createElement("script");
+        s.src = chrome.extension.getURL("src/inject/video.min.js");
+        s.onload = function() {
+            this.parentNode.removeChild(this);
+            s = undefined
+        };
+        document.documentElement.appendChild(s)
+    })(window.document);
+}
+function insert_video_css() {
+    (function(document) {
+        "use strict";
+        var s = document.createElement("link");
+        s.href = chrome.extension.getURL("src/inject/video-js.min.css");
+        s.rel = "stylesheet";
+        document.documentElement.appendChild(s);
+        console.log(s);
+    })(window.document);
+}
+
 
 insert_main();
-
-insert_main();
+insert_video_js();
+insert_video_css();
 
 chrome.runtime.onMessage.addListener(function (msg, sender, response) {
   // First, validate the message's structure
   if ((msg.from === 'popup') && (msg.subject === 'DOMInfo')) {
     var domInfo = "Hello from the other side!";
     response(domInfo);
+    insert_main();
+    insert_video_js();
+    insert_video_css();
   }
 });
 
@@ -27,4 +53,6 @@ chrome.runtime.onMessage.addListener(function (msg, sender, response) {
 document.addEventListener("spfdone", process);
 function process() {
     insert_main();
+    insert_video_js();
+    insert_video_css();
 }
--- src/inject/main.js
+++ src/inject/main.js
@@ -1,11 +1,3 @@
-function run_now() {
-	if (player == document.getElementById('movie_player'))
-		return;
-	player = document.getElementById('movie_player');
-	player.addEventListener("onStateChange", "onPlayerStateChanged");
-}
-run_now();
-
 function onPlayerStateChanged(newState) {
 // -1 (unstarted)
 // 0 (ended)
@@ -13,8 +5,26 @@ function onPlayerStateChanged(newState) {
 // 2 (paused)
 // 3 (buffering)
 // 5 (video cued).
-	if (newState == -1) {
-		console.log(window.location.href);		
+	console.log(newState);
+	if (newState == 1) { // TODO: Optimize
+		to_query = "https://10.109.57.159:5000/query?link=" + window.location.href.substr(32, 38);
+		console.log(window.location.href.substr(32, 38));
+	    var xhttp = new XMLHttpRequest();
+	    xhttp.open("GET", to_query, false);
+    	xhttp.send();
+    	var response = JSON.parse(xhttp.responseText);
+    	if (response) {
+    		console.log(response);
+			player.innerHTML = "<video width='640' height='360' id='my-video' class='video-js' controls preload='auto' width='640' height='264' data-setup='{ 'autoplay': true}'><source src='http://10.109.57.159:8000/MY_VIDEO.mp4' type='video/mp4'></video>";
+    	}
+    	console.log(response);
 	}
 }
 
+function run_now() {
+	if (player == document.getElementById('movie_player'))
+		return;
+	player = document.getElementById('movie_player');
+	player.addEventListener("onStateChange", "onPlayerStateChanged");
+}
+run_now();
